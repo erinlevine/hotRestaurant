b58bf44eac01d3f34aa7b1bd28ffb94ea3e249cf
erinlevine <erinclevine@gmail.com>
2017-06-27 14:58:23 -0400

    Front end done. Piecing together back end

---
+++ server.js
@@ -0,0 +1,94 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Star Wars Characters (DATA)
+// =============================================================
+var table = [{
+  name: "Yoda",
+  phone: "555-555-5555",
+  email: "yoda@yoda.com",
+  id: 2000
+}, {
+  name: "Darth Maul",
+  phone: "555-555-5555",
+  email: "yoda@yoda.com",
+  id: 2000
+}, {
+  name: "Vader",
+  phone: "555-555-5555",
+  email: "yoda@yoda.com",
+  id: 2000
+}, {
+  name: "Luke",
+  phone: "555-555-5555",
+  email: "yoda@yoda.com",
+  id: 2000
+}, {
+  name: "Obi Wan Kenobi",
+  phone: 555-555-5555,
+  email: "yoda@yoda.com",
+  id: 2000
+}];
+
+var waitlist = [];
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "root.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newcharacter = req.body;
+
+  console.log(newcharacter);
+  if (table.length < 5) {
+    table.push(newcharacter);
+  }
+
+  else {
+    waitlist.push(newcharacter);
+  }
+  
+
+  res.json(newcharacter);
+});
+
+app.get("/api/table", function(req, res) {
+ 
+    res.json(table);
+  
+});
+
+app.get("/api/waitlist", function(req, res) {
+
+    
+    res.json(waitlist);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
